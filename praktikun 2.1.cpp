d97c10bea19a23821718f45ead7c5074b624da09
naufal1214 <2400018226@webmail.uad.ac.id>
2025-04-15 14:59:52 +0700

    Add files via upload

---
+++ praktikun 2.1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    float uangku;
+    cout<<"Berapa uang ku =";
+    cin>>uangku;
+    if(uangku>=75000)
+     cout<<"Sepatu baru\n";
+     }
